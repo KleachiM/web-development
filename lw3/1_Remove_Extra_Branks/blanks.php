ef61f6e280061f3c8e86a1d956f9a2c1935ae28a
KleachiM <werf121@rambler.ru>
2022-02-27 09:00:17 +0300

    added lw3/1/Удаление лишних пробелов

---
+++ lw3/1_Remove_Extra_Branks/blanks.php
@@ -0,0 +1,23 @@
+<?php
+
+function getGetParameter(string $name): ?string
+{
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
+
+header('Content-Type: text/plain');
+
+$text = getGetParameter('text');
+if ($text !== null) {
+	$trimmedText = trim($_GET['text']);	// удаление ' ' в начале и в конце строки
+	$arr = explode(' ', $trimmedText);	// запись в массив разбивая по ' '
+	$tmpArr = [];	// временный массив для записи непробельных символов
+	for ($i=0; $i < count($arr); $i++) { 
+		if ($arr[$i] !== '')
+			$tmpArr[] = $arr[$i];
+	}
+	$stringWithounBlanks = implode('#', $tmpArr);  // преобразование массива в строку
+	echo $stringWithounBlanks;
+} else {
+	echo 'No such parameter: text';
+}
