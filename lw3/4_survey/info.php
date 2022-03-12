e6f36abce9024c0db3a3abe09efb1f188831dfc6
KleachiM <werf121@rambler.ru>
2022-03-12 10:45:11 +0300

    Added survey info on lw_3

---
+++ lw3/4_survey/info.php
@@ -0,0 +1,25 @@
+<?php
+
+header('Content-Type: text/plain');
+
+$mail = getGetParameter('email');
+
+if ($mail === null) {
+	echo null;
+} else {
+	$file = "./data/$mail.txt";
+
+	if (file_exists($file)) {
+		$f = fopen($file, 'r');
+		$txt = fread($f, filesize($file));
+		fclose($f);
+		echo $txt;
+	} else {
+		echo "No such file";
+	}
+}
+
+function getGetParameter(string $name): ?string
+{
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
--- lw3/4_survey/survey.php
+++ lw3/4_survey/survey.php
@@ -21,8 +21,6 @@ if ($mail === null) {
 		$patternAge = "/(Age: )(\w*)/";
 
 		if ($firstName) {	// если в запросе указано имя - меняется имя
-			// $arg = $firstName;
-			// $txt = preg_replace_callback($patternFirstName, 'callBackReplace', $txt);
 			$txt = preg_replace_callback(
 				$patternFirstName, 
 				function ($matches) use ($firstName) { return $matches[1] . $firstName; }, 
@@ -30,8 +28,6 @@ if ($mail === null) {
 		}
 
 		if ($lastName) {	// если в запросе указана фамилия - меняется фамилия
-			// $arg = $lastName;
-			// $txt = preg_replace_callback($patternLastName, 'callBackReplace', $txt);
 			$txt = preg_replace_callback(
 				$patternLastName, 
 				function ($matches) use ($lastName) { return $matches[1] . $lastName; }, 
@@ -39,15 +35,12 @@ if ($mail === null) {
 		}
 
 		if ($age) {		// если в запросе указан возраст - меняется возраст
-			// $arg = $age;
-			// $txt = preg_replace_callback($patternAge, 'callBackReplace', $txt);
 			$txt = preg_replace_callback(
 				$patternAge, 
 				function ($matches) use ($age) { return $matches[1] . $age; }, 
 				$txt);
 		}
 
-		// $res = preg_replace_callback($patternLastName, 'callBackReplace', $txt);
 		$f = fopen($file, 'w');
 		fwrite($f, $txt);
 		fclose($f);
@@ -62,12 +55,6 @@ function getGetParameter(string $name): ?string
 {
     return isset($_GET[$name]) ? $_GET[$name] : null;
 }
-
-// function callBackReplace($matches, $arg)
-// {
-// 	$val = $matches[1] . $arg;
-// 	return $val;
-// }
 
 function getSubstringRegEx(string $pattern, string $text): ?string
 {
